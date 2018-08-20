be3ce2a6346794e43c4c912bed9907d2d5541d3d
Rhydian Thomas <rhydiant@gmail.com>
2018-08-20 15:27:22 +1000

    Documented eslint exceptions

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,9 @@ module.exports = {
   extends: 'airbnb',
   rules: {
     strict: 0,
+    /// reactnative doesn't support .jsx extentsions
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    /// avoid boilerplate propTypes for react-navigation props
     'react/prop-types': ['error', { ignore: ['navigation'] }],
   },
 };
